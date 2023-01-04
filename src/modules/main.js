75dd7a1cc956e492b1e17c4c8dc6827abf0b5724
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2023-01-04 19:41:44 +0200

    moving internet shop to internal folder

--- src/modules/main.js
+++
@@ -1,9 +0,0 @@
-const dataVizualizer = require("./dataVizualizer.js");
-const { initCount } = require("./basketLogic.js");
-const { buildNav } = require( "./navHelper.js");
-
-document.addEventListener("DOMContentLoaded", () => {
-    dataVizualizer.listProducts();
-    initCount();
-    buildNav();
-});
--- src/modules/mainBasket.js
+++
@@ -1,9 +0,0 @@
-const dataVizualizer = require("./dataVizualizer.js");
-const { buildNav } = require("./navHelper.js");
-const { initCount } = require("./basketLogic");
-
-document.addEventListener("DOMContentLoaded", () => {
-    initCount();
-    dataVizualizer.listBasketProducts();
-    buildNav();
-});
